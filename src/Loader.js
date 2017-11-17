6b8f1ea9fb5973fd2e1a7e1177f474a85f3503d7
Amiria <amiriaknd@outlook.com>
2017-11-17 09:03:07 +0800

    读取PNG宽高

--- src/Loader.js
+++ src/Loader.js
@@ -14,11 +14,17 @@ class Loader {
                 .sort((fna, fnb) => (fna - fnb))
                 .map(function(filename) {
                     const file = zip.files[filename]
+                    const width = U8toInt(file._data.compressedContent.subarray(16, 20))
+                    const height = U8toInt(file._data.compressedContent.subarray(20, 24))
                     return file.async('blob').then((blob) => (URL.createObjectURL(blob)))
                 })
             return Promise.all(promises)
         })
     }
+}
+
+function U8toInt(u8) {
+    return (u8[0] << 24 | u8[1] << 16 | u8[2] << 8 | u8[3])
 }
 
 export default Loader
